a87678051ac19afb69ef0e823713f36bd3c2af1b
MacBook <macbook@MacBook-Pro-MacBook.local>
2022-10-01 23:36:14 +0300

    add styles

--- layout/menu.php
+++ layout/menu.php
@@ -3,6 +3,7 @@
         <div class="header__inner">
 
             <nav class="nav">
+                <a class="nav__link" href="index.php">Main</a>
                 <a class="nav__link" href="index.php?action=task">Task</a>
             </nav>
         </div>
